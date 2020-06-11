a78e8bcf8b3ee7db8ce0a5d11c35ffe5701e497b
Asad Khan <asad_khan47@hotmail.co.uk>
2020-06-11 17:58:36 +0100

    Improving readability

--- src/main/java/com/mycompany/variableconfig/VariableConfig.java
+++ src/main/java/com/mycompany/variableconfig/VariableConfig.java
@@ -14,8 +14,8 @@ public final class VariableConfig {
     public static final String URL_USER_FOR_ID = "https://bpdts-test-app.herokuapp.com/user/%s";
     public static final String URL_USERS_FOR_CITY_OF = "https://bpdts-test-app.herokuapp.com/city/%s/users";
     public static final String FIFTY_MILE_MESSAGE = "All users with co-ordinates within 50 miles of London are: ";
-    public static final String USERS_FOR_CITY_MESSAGE = "All users with city as London: ";
-    public static final String USER_WITH_ID_MESSAGE = "User with user id " + USER_ID + ";";
+    public static final String USERS_FOR_CITY_MESSAGE = "All users with their city as London are: ";
+    public static final String USER_WITH_ID_MESSAGE = "User with user id " + USER_ID + ": ";
     public static final String LONDON = "London";
     public static final String LATITUDE = "latitude";
     public static final String LONGITUDE = "longitude";
